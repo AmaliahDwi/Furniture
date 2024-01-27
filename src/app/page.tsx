6a459744f2c66c4a9b191751cf8f899019076785
MhdIhsanPohan <pohan488@gmail.com>
2024-01-27 11:26:48 +0700

    error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,24 +27,7 @@ export default function Home() {
     <main className="bg-white">
       <Layout>
         <div>
-          <Carousel
-            swipeable={false}
-            draggable={false}
-            showDots={true}
-            responsive={responsive}
-            ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            autoPlay={this.props.deviceType !== "mobile" ? true : false}
-            autoPlaySpeed={1000}
-            keyBoardControl={true}
-            customTransition="all .5"
-            transitionDuration={500}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet", "mobile"]}
-            deviceType={this.props.deviceType}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-          >
+          <Carousel responsive={responsive}>
             <Image
               src="/gambar/iklan 1.png"
               width={600}
