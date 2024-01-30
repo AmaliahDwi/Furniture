73903a25b16cad3f4f985dddddab6ad983fab90e
MhdIhsanPohan <pohan488@gmail.com>
2024-01-30 17:07:20 +0700

    Merge remote changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Layout from "./components/Layout";
 import Hero from "./components/page/Hero";
+import Banner from "./components/page/Banner";
 
 export default function Home() {
   return (
