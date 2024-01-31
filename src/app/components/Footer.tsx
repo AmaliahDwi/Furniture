236a7b1513aa3fbb36847e9db2bf5d3d005a2d98
MhdIhsanPohan <pohan488@gmail.com>
2024-01-31 14:58:24 +0700

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,166 +1,64 @@
 import React from "react";
 import { AiFillInstagram } from "react-icons/ai";
-import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaTelegramPlane } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { BsFillTelephoneFill } from "react-icons/bs";
 import Image from "next/image";
 
 function Footer({ fullscreen }: any) {
   return (
     <div className="py-5 bg-gray-100 rounded-t-[30px] border-t border-lg border-gray-200">
-      <div className="grid grid-cols-3 gap-4 justify-items-center">
-        <div className="flex-row w-72 mb-6">
-          <div className="text-gray-500 text-lg font-medium text-center mb-4">
-            Metode Pembayaran
-          </div>
-          <div className="grid grid-cols-3 gap-x-20 gap-y-10 justify-items-center items-center">
-            <Image
-              src={"/gambar/visa.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-              className="text-center"
-            ></Image>
-            <Image
-              src={"/gambar/mastercard.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/atm bersama.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/bank transfer.png"}
-              width={40}
-              height={20}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/dana.png"}
-              width={50}
-              height={25}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/jcb.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/ovo.png"}
-              width={50}
-              height={25}
-              alt="lamp"
-            ></Image>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="">
+          <div className="flex-row w-72 mb-6">
+            <h1 className="text-lg">Company</h1>
+            <div className="flex-row text-left ">
+              <h2>- About us</h2>
+              <h2>- Contact Us</h2>
+              <h2>- Privacy Policy</h2>
+              <h2>- Terms & Condition</h2>
+              <h2>- Suport Centre</h2>
+            </div>
           </div>
         </div>
         <div className="flex-row w-72 mb-6">
-          <div className="text-gray-500 text-lg font-medium text-center mb-4">
-            Jasa Pengiriman
-          </div>
-          <div className="grid grid-cols-3 gap-x-20 gap-y-10 justify-items-center items-center">
-            <Image
-              src={"/gambar/jne.png"}
-              width={45}
-              height={23}
-              alt="lamp"
-              className="text-center"
-            ></Image>
-            <Image
-              src={"/gambar/jnt.png"}
-              width={60}
-              height={30}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/lion.png"}
-              width={70}
-              height={35}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/pos.png"}
-              width={50}
-              height={25}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/sicepat.png"}
-              width={70}
-              height={35}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/tiki.png"}
-              width={60}
-              height={30}
-              alt="lamp"
-            ></Image>
+          <div className="text-lg">Question?</div>
+          <div className="flex-row text-left ">
+            <h2>- Help Support</h2>
+            <h2>- Track Order</h2>
+            <h2>- Return</h2>
+            <h2>- Shipping Info</h2>
+            <h2>- History</h2>
           </div>
         </div>
         <div className="flex-row w-72 mb-6">
-          <div className="text-gray-500 text-lg font-medium text-center mb-4">
-            Metode Pembayaran
+          <div className="text-lg">Useful Links</div>
+          <div className="flex-row text-left ">
+            <h2>- Gift Cards</h2>
+            <h2>- Size Chart</h2>
+            <h2>- My Account</h2>
+            <h2>- Our Locations</h2>
+            <h2>- FAQs</h2>
           </div>
-          <div className="grid grid-cols-3 gap-x-20 gap-y-10 justify-items-center items-center">
-            <Image
-              src={"/gambar/visa.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-              className="text-center"
-            ></Image>
-            <Image
-              src={"/gambar/mastercard.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/atm bersama.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/bank transfer.png"}
-              width={40}
-              height={20}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/dana.png"}
-              width={50}
-              height={25}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/jcb.png"}
-              width={30}
-              height={15}
-              alt="lamp"
-            ></Image>
-            <Image
-              src={"/gambar/ovo.png"}
-              width={50}
-              height={25}
-              alt="lamp"
-            ></Image>
+        </div>
+        <div className="flex-row w-72 mb-6">
+          <div className="text-lg">Connect With us</div>
+          <div className="flex-row text-left ">
+            <div className="flex gap-4 p-2">
+              <AiFillInstagram className="w-4 h-4  text-zinc-900" />
+              <FaFacebookF className="w-4 h-4 text-zinc-900" />
+              <FaTwitter className="w-4 h-4 text-zinc-900" />
+            </div>
+            <div className="flex-row justify-between">
+              <FaTelegramPlane className="flex gap-2" />
+              <div className="flex">7889 Ljkhf</div>
+              <MdEmail /> wbfdgjkgf
+              <BsFillTelephoneFill /> fgbjkgf
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="lg:text-2xl text-xl text-center text-slate-800">
-        Find us in social media
-      </div>
-      <div className="flex gap-4 justify-center p-4">
-        <AiFillInstagram className="w-6 h-6  text-zinc-900" />
-        <FaFacebookF className="w-6 h-6 text-zinc-900" />
-        <FaTwitter className="w-6 h-6 text-zinc-900" />
-      </div>
       <div className="text-gray-700 text-center sm:text-sm lg:text-base">
         &copy; 2024 Abani Furniture all rights reserved
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="bg-white/50">
       <Layout>
-        <Banner />
+        {/* <Banner /> */}
 
         <Hero />
 
